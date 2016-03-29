075ef5878cdf2b367668e25c240d370689e5f38f
Zygonie <github@zygonie.com>
2016-03-29 15:34:05 -0400

    rebase sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+import time
+from logging.handlers import TimedRotatingFileHandler
 from scapy.all import *
+from pymongo import MongoClient
+import os
 
 def pkt_handler(pkt):
-  if pkt.haslayer(Dot11):
-    hwAddr = pkt.addr2
-    signal = ord(pkt.notdecoded[-4:-3])-256
-    if pkt.type==0 and pkt.subtype==4:
-      print "MAC {0} with Signal {1}dBm".format(hwAddr, signal)
+    if pkt.haslayer(Dot11):
+        hwAddr = pkt.addr2
+        signal = ord(pkt.notdecoded[-4:-3])-256
+        if pkt.type==0 and pkt.subtype==4:
+            now = time.time()
+            str = '{{"ts":{}, "moduleId":"{}", "uuid":"{}","dBm_signal":{}}}'.format(now, moduleId, hwAddr, signal)
+            logger.info(str)
 
-sniff(iface="mon1", prn=pkt_handler)
+
+formatter = logging.Formatter("%(asctime)s -- %(message)s")
+logger = logging.getLogger("nanalytics")
+logger.setLevel(logging.INFO)
+handler = logging.handlers.TimedRotatingFileHandler(
+    "probe_request.log",
+    when="s",
+    interval=15,
+    encoding="utf-8",
+    utc=True)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+moduleId = "RPi-1"
+
+sniff(iface="mon0", prn=pkt_handler) 
